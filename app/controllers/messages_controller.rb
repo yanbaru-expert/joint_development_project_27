6587d009c38a1736c60a6a61685f8635f6a414e8
Katsuya Minami <arms.mc.rp@gmail.com>
2020-08-09 23:33:23 +0900

    新規投稿機能を実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,3 +1,16 @@
 class MessagesController < ApplicationController
 
+  def new
+    @message = Message.new
+  end
+
+  def create
+    Message.create(message_params)
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:title, :content)
+  end
+  
 end
