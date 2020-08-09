c678db6538a7caf3430934c6d4e0c632519f7a48
hisadomi <hb.kmaia93@icloud.com>
2020-08-10 08:18:05 +0900

    新規登録機能作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,14 @@
 class TasksController < ApplicationController
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(task_params)
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :content)
+  end
 end
