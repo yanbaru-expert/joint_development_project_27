833cb4a3620167616e3e3dcdd59ecb8a74a05b29
Urimoto-Takaaki <69062224+Urimoto-Takaaki@users.noreply.github.com>
2020-08-10 17:14:26 +0900

    詳細表示機能 show を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,16 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  
   def new
     @user = User.new
   end
 
   def create
     User.create(user_params)
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   private
