a5d6d47e8eea6047023512a56b2c8ad11cb0c6ea
hisadomi <hb.kmaia93@icloud.com>
2020-08-10 20:33:37 +0900

    詳細表示機能追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,7 +10,11 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
-  
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
   private
   def task_params
     params.require(:task).permit(:title, :content)
